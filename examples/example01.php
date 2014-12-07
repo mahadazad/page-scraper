107816466fd6e89200ebaaf358bbe8c26ecda0b9
Muhammad Mahad Azad <muhammadmahadazad@Muhammads-MacBook-Pro.local>
2014-12-07 22:24:35 +0500

    added new examples

--- examples/example01.php
+++ examples/example01.php
@@ -1,20 +1,28 @@
 <?php
 
+libxml_use_internal_errors(true);
+
 include __DIR__ . '/../vendor/autoload.php';
 
 use PageScrapper\Client;
 
+// the most simpliest way to fetch the data is to use the Client
 $client = new Client(array(
+    // the html/xml that we are interested in
     'url'         => 'https://news.ycombinator.com',
+    // the data we are interested in
     'data_config' => array(
-        'titles' => '//td[@class="title"]//a/text()',
-        'links' => '//td[@class="title"]//a/@href',
+        'titles' => '//td[@class="title"]//a/text()', // the xpath query
+        'links' => '//td[@class="title"]//a/@href', // the xpath query
     ),
 ));
 
+// fetch the data, and get the Page object
 $page = $client->fetchPage();
+// get the desired data
 $data = $page->getData();
 
+// print the data
 echo '<pre>';
 print_r(array_combine(
     $data['titles'],
